0125f3a24124b4d8c6144aa179917dbd3055102c
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-10-28 20:14:12 -0500

    Generador actualizado.

--- semana-8/generador-de-usuarios/assets/js/scripts.js
+++ semana-8/generador-de-usuarios/assets/js/scripts.js
@@ -1,1 +1,11 @@
-console.log('faker ', faker)
+let productos = ''
+
+for (let i = 0; i < 20; i++) {
+  const color = faker.commerce.color()
+  const nombre = faker.commerce.product()
+  const precio = faker.commerce.price()
+
+  productos += `{ nombre: "${nombre}", color: "${color}", precio: ${precio} },`
+}
+
+console.log(productos)
